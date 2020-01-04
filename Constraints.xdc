59b9c1a963c578e0fdfa816c2b946b204e952cce
ffy062 <56193560+ffy062@users.noreply.github.com>
2020-01-04 22:23:39 +0800

    Add files via upload

--- Constraints.xdc
+++ Constraints.xdc
@@ -9,10 +9,10 @@ set_property IOSTANDARD LVCMOS33 [get_ports clk]
 create_clock -period 10.000 -name sys_clk_pin -waveform {0.000 5.000} -add [get_ports clk]
 
 ## Switches
-#set_property PACKAGE_PIN V17 [get_ports {sw[0]}]
-#set_property IOSTANDARD LVCMOS33 [get_ports {sw[0]}]
-#set_property PACKAGE_PIN V16 [get_ports {sw[1]}]
-#set_property IOSTANDARD LVCMOS33 [get_ports {sw[1]}]
+set_property PACKAGE_PIN V17 [get_ports {sw[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {sw[0]}]
+set_property PACKAGE_PIN V16 [get_ports {sw[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {sw[1]}]
 #set_property PACKAGE_PIN W16 [get_ports {sw[2]}]
 #set_property IOSTANDARD LVCMOS33 [get_ports {sw[2]}]
 #set_property PACKAGE_PIN W17 [get_ports {sw[3]}]
@@ -108,16 +108,16 @@ set_property IOSTANDARD LVCMOS33 [get_ports {an[3]}]
 
 
 ##Buttons
-set_property PACKAGE_PIN U18 [get_ports start]
-set_property IOSTANDARD LVCMOS33 [get_ports start]
-set_property PACKAGE_PIN T18 [get_ports rst]
-set_property IOSTANDARD LVCMOS33 [get_ports rst]
-#set_property PACKAGE_PIN W19 [get_ports btnL]
-#set_property IOSTANDARD LVCMOS33 [get_ports btnL]
-set_property PACKAGE_PIN T17 [get_ports stop]
-set_property IOSTANDARD LVCMOS33 [get_ports stop]
-#set_property PACKAGE_PIN U17 [get_ports rst]
-#set_property IOSTANDARD LVCMOS33 [get_ports rst]
+set_property PACKAGE_PIN U18 [get_ports btnC]
+set_property IOSTANDARD LVCMOS33 [get_ports btnC]
+set_property PACKAGE_PIN T18 [get_ports btnU]
+set_property IOSTANDARD LVCMOS33 [get_ports btnU]
+set_property PACKAGE_PIN W19 [get_ports btnL]
+set_property IOSTANDARD LVCMOS33 [get_ports btnL]
+set_property PACKAGE_PIN T17 [get_ports btnR]
+set_property IOSTANDARD LVCMOS33 [get_ports btnR]
+set_property PACKAGE_PIN U17 [get_ports btnD]
+set_property IOSTANDARD LVCMOS33 [get_ports btnD]
 
 
 
--- finite_state_machine.v
+++ finite_state_machine.v
@@ -1,75 +1,132 @@
 // finite state machine and score scounter
 
-`define rst 3'd0 
-`define b_rst 3'd1
-`define b_play 3'd2
-`define play 3'd3
-`define finish 3'd4 
+`define rst 4'd0 
+`define b_rst 4'd1
+`define b_play 4'd2
+`define stage1 4'd3
+`define stage2 4'd4
+`define stage3 4'd5
+`define pmode 4'd6
+`define win 4'd7
+`define lose 4'd8
+`define finish 4'd9
 
 
 
 module fsm(
     clk, 
-    rst, start, stop, 
+    rst, start, stop, pmode, back,
     goal, 
-    dig0, dig1, dig2, dig3, cur_state,
+    dig0, dig1, dig2, dig3, c_state,
     pmod1, pmod2, pmod4
     );
 
-input clk, rst, start, goal, stop;
+input clk;
+input rst, start, stop, pmode, back;
+input goal;
 output reg [3:0] dig0, dig1, dig2, dig3;
-output reg [2:0] cur_state;
+output reg [3:0] c_state;
 output pmod1, pmod2, pmod4;
 
-reg [2:0] state, n_state;
+reg [3:0] state, n_state;
 reg [3:0] cnt_d0, cnt_d1, n_cnt_d0, n_cnt_d1;
 wire [3:0] score0, score1;
 wire en_cnt, en_goal, cnt_play;
 reg cnt_start, sp;
 
+
+// DFF of state
 always@ (posedge clk) begin
     state <= n_state;
-    cur_state <= state;
+    c_state <= state;
 end
 
+
+// DFF of output number
 always@ (posedge clk) begin
     cnt_d0 <= n_cnt_d0;
     cnt_d1 <= n_cnt_d1;
 end
 
+
+// always block to control state
 always@(*) begin
     if(rst == 1'b1) begin
         n_state = `b_rst;
     end
     else begin
         case(state)
-            `rst: n_state = (start == 1'b1)? `b_play : `rst;
+            `rst: begin
+                if(start == 1'b1) begin
+                    n_state = `b_play;
+                end
+                else begin
+                    if(pmode == 1'b1) begin
+                        n_state = `pmode;
+                    end
+                    else begin
+                        n_state = `rst;
+                    end
+                end
+            end
             `b_rst: n_state = (cnt_d0 == 4'd0)? `rst : `b_rst;
-            `b_play: n_state = (cnt_d0 == 4'd5)? `play : `b_play;
-            `play: n_state = (cnt_d1 == 4'd0 && cnt_d0 == 4'd0)? `finish : `play;
-            `finish: n_state = (start == 1'b1)? `b_play : `finish;
+            `b_play: n_state = (cnt_d0 == 4'd5)? `stage1 : `b_play;
+            `stage1: begin
+                if(cnt_d1 == 4'd0 && cnt_d0 == 4'd0) begin
+                    n_state = `finish;
+                end
+                else begin
+                    if(back == 1'b1) begin
+                        n_state = `rst;
+                    end
+                    else begin
+                        n_state = `stage1;
+                    end
+                end
+            end
+            `pmode: n_state = (back == 1'b1)? `rst : `pmode;
+            `finish: begin
+                if(start == 1'b1) begin
+                    n_state = `b_play;
+                end
+                else begin
+                    if(back == 1'b1) begin
+                        n_state = `rst;
+                    end
+                    else begin
+                        n_state = `finish;
+                    end
+                end
+            end
             default: n_state = `rst;
         endcase
     end
 end
 
+// always block to control game play/ pause
 always@(posedge clk) begin
     if(rst == 1'b1) begin
         sp <= 1'b0;
     end
     else begin
-        sp <= (stop == 1'b1 && state == `play)? ~sp : sp;
+        sp <= (stop == 1'b1 && state == `stage1)? ~sp : sp;
     end
 end
 
-assign dis_score = (state == `play || state == `finish)? 1'b1 : 1'b0;
-assign en_goal = (state == `play && goal == 1'b1 && sp == 1'b0)? 1'b1 : 1'b0;
+
+// signal for score display, valid goal, play count down sound
+assign dis_score = (state == `stage1 || state == `finish || state == `pmode)? 1'b1 : 1'b0;
+assign en_goal = ((state == `stage1 || state == `pmode) && goal == 1'b1
+            && sp == 1'b0)? 1'b1 : 1'b0;
 assign cnt_play = (cnt_start == 1'b1 && n_state == `b_play)? 1'b1 : 1'b0;
 
+// module for 1 sec counter, currnet score to seven segment, audio top module
 counter one_sec(.clk(clk), .start(cnt_start), .stop(sp), .out(en_cnt));
 score_and_display sad(.clk(clk), .goal(en_goal), .dis_score(dis_score), .score0(score0), .score1(score1));
 audio_top audio(.clk(clk), .rst(rst), .goal(en_goal), .cnt(cnt_play), .pmod_1(pmod1), .pmod_2(pmod2), .pmod_4(pmod4));
 
+
+// always block for count down signal
 always@(*) begin
     if(rst == 1'b1) begin
         n_cnt_d0 = 4'd1;
@@ -77,7 +134,7 @@ always@(*) begin
         cnt_start = 1'b1;
     end
     else begin
-        if(start == 1'b1 && state != `play) begin
+        if(start == 1'b1 && state != `stage1) begin
             n_cnt_d0 = 4'd3;
             n_cnt_d1 = 4'd0;
             cnt_start = 1'b1;
@@ -86,7 +143,7 @@ always@(*) begin
             if(en_cnt == 1'b1) begin
                 if(cnt_d0 == 4'd1 && state == `b_play) begin
                     n_cnt_d0 = 4'd5;
-                    n_cnt_d1 = 4'd1;
+                    n_cnt_d1 = 4'd3;
                 end
                 else begin
                     n_cnt_d0 = (cnt_d0 == 4'd0)? 4'd9 : cnt_d0 - 1;
@@ -103,6 +160,8 @@ always@(*) begin
     end
 end
 
+
+// always block to determine signal to display on seven segmnet
 always@(*) begin
     case(state)
         `rst: begin
@@ -123,11 +182,17 @@ always@(*) begin
                 dig2 = cnt_d0;
                 dig3 = 4'd11;
         end
-        `play: begin
+        `stage1: begin
                 dig0 = score0;
                 dig1 = score1;
                 dig2 = cnt_d0;
                 dig3 = cnt_d1;
+        end
+        `pmode: begin
+                dig0 = score0;
+                dig1 = score1;
+                dig2 = 4'd10;
+                dig3 = 4'd10;
         end
         `finish: begin
                 dig0 = score0;
